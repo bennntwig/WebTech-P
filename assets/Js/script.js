f301299d2bc28f34ca1ffc90e3b457e6dc319fcc
der commit auf deine schwester <jan26341@gmail.com>
2025-01-09 11:40:15 +0100

    jsscript

---
+++ assets/Js/script.js
@@ -0,0 +1,8 @@
+
+function getViewportWidth() {
+    return window.innerWidth || document.documentElement.clientWidth;
+}
+
+
+const viewportWidth = getViewportWidth();
+console.log(`Die Viewport-Breite beträgt: ${viewportWidth} Pixel.`);
