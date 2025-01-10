8f9ed27c99bda6d86fc52528882d38355d646fe0
bennntwig <bennntwig@gmail.com>
2025-01-10 13:30:15 +0100

    Praktikum 8 Aufgabe 1

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -5,3 +5,63 @@ function getViewportWidth() {
 
 const viewportWidth = getViewportWidth();
 console.log(`Die Viewport-Breite beträgt: ${viewportWidth} Pixel.`);
+
+
+
+class Kategorie {
+    constructor(name, bildUrl, bildName) {
+        this.name = name;
+        this.bild = function () {
+            return new Bild(bildUrl, bildName);
+        };
+    }
+}
+
+
+class Bild {
+    constructor(url, name) {
+        this.url = url;
+        this.name = name;
+    }
+}
+
+class Tutorial {
+    constructor(name, sprache, beschreibung, dauer, datum, url, embedCode, kapitel, bildUrl, bildName) {
+        this.name = name;
+        this.sprache = sprache;
+        this.beschreibung = beschreibung;
+        this.dauer = dauer;
+        this.datum = datum;
+        this.url = url;
+        this.embedCode = embedCode;
+        this.kapitel = [];
+        this.kategorienliste = [];
+        this.bild = function () {
+            return new Bild(bildUrl, bildName);
+        }
+    }
+
+    fuegeKategorieHinzu(kat) {
+        this.kapitel.push(kat);
+    }
+
+    fuegeKapitelHinzu(kap) {
+        this.kategorienliste.push(kap);
+    }
+}
+
+class Kapitel {
+    constructor(name, beschreibung, dauer) {
+        this.name = name;
+        this.beschreibung = beschreibung
+        this.dauer = dauer;
+    }
+}
+
+
+function getDauerInStundenUndMinuten(dauer) {
+    let std = dauer.charAt(0) + charAt(1);
+    let min = dauer.charAt(3) + charAt(4);
+
+    return std + " Std." + min + " Min.";
+}
