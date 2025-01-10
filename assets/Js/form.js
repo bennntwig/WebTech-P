68d780f8c4bfcd53da63fffdec2029a12fdcf638
bennntwig <bennntwig@gmail.com>
2025-01-10 16:17:26 +0100

    Blatt 9 Aufgabe 1 (funktioniert) (ich bin dumm)

--- assets/Js/form.js
+++ assets/Js/form.js
@@ -1,19 +1,24 @@
-
+let videoRadio = document.getElementById("videoRadio");
+let textRadio = document.getElementById("textRadio");
 
 let wertTextArea = document.createElement("textarea");
 wertTextArea.id = "Wert";
 wertTextArea.name = "Content";
 wertTextArea.required = true;
 wertTextArea.placeholder = "YouTube Embed Code des Tutorials";
-wertTextArea.onclick(document.getElementById("Wert").replaceWith(wertTextArea));
-wertTextArea.onclick(document.getElementById("dauer").parentNode.class = "notRequired");
+videoRadio.onclick = function () {
+    document.getElementById("Wert").replaceWith(wertTextArea);
+    document.getElementById("dauer").parentNode.className = "required";
+};
 
-let wertInputText = docoument.createElement("input");
+let wertInputText = document.createElement("input");
 wertInputText.type = "text";
 wertInputText.id = "Wert";
 wertInputText.name = "Content";
 wertInputText.required = true;
 wertInputText.placeholder = "Link zum Tutorial";
-wertInputText.onclick(document.getElementById("Wert").replaceWith(wertInputText));
-wertTextArea.onclick(document.getElementById("dauer").parentNode.class = "required");
 
+textRadio.onclick = function () {
+    document.getElementById("Wert").replaceWith(wertInputText);
+    document.getElementById("dauer").parentNode.className = "notRequired";
+}
